2cb3d4d83bb39eb9d286deb2094fc992e8c091e3
Gaurav Kolekar <gaurav.kolekar01@gmail.com>
2024-01-07 00:58:21 +0000

    tested mock for requests get

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import src.service as service
 import unittest.mock as mock
 
@@ -7,3 +8,21 @@ def test_get_user_from_db(mock_get_user_from_db):
     mock_get_user_from_db.return_value = "Mocked Alice"
     username = service.get_user_from_db(1)
     assert username == "Mocked Alice"
+
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"id": 1, "name": "John Doe"}
+    mock_get.return_value = mock_response
+    data = service.get_users()
+    assert data == {"id": 1, "name": "John Doe"}
+
+@mock.patch("requests.get")
+def test_get_users_error(mock_get):
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    mock_get.return_value = mock_response
+    with pytest.raises(requests.HTTPError):
+        service.get_users()
+
